a16bf43d240feded66c46beb8e6e502303591ddd
hillary <781915596@qq.com>
2019-03-02 17:05:38 +0800

    链接整合

---
+++ js/YleoStyle.js
@@ -0,0 +1,15 @@
+ $(function () {            
+            $(".btnstyle,.btn1,.btn2").click(function () {
+                $(this).toggleClass('liked');                
+            })
+        })
+ $(function () {
+ 	$(".shop_car").click(function () {
+ 		window.location.href="支付界面.html";
+ 	})
+ })
+  $(function () {
+ 	$(".logo").click(function () {
+ 		window.location.href="着陆页.html";
+ 	})
+ })
