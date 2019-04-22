4ac4c89b6d4a9e38f6abee032cf7cf13c93886b9
hillary <781915596@qq.com>
2019-04-22 20:59:32 +0800

    更新了css和js

--- js/YleoStyle.js
+++ js/YleoStyle.js
@@ -13,3 +13,16 @@
  		window.location.href="着陆页.html";
  	})
  })
+   $(function () {
+ 	$(".details").click(function () {
+ 		window.location.href="商品详情.html";
+ 	})
+ })
+ //     $(function () {
+ // 	$(".numlook.btn-tra1").click(function () {
+ // 		$(".numlook.output-tra").HTML;
+ // 	});
+ // 	$(".numlook.btn-tra2").click(function(){
+ // 		$(".numlook.output-tra")++;
+ // 	})
+ // })
